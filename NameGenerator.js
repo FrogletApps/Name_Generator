634dd1d22ee3ddc475c895ab2597f4506e542895
James Pearson <jap61@kent.ac.uk>
2018-08-22 22:02:11 +0100

    Added names
    Basically just a starting point

--- NameGenerator.js
+++ NameGenerator.js
@@ -1,8 +1,18 @@
-//Generates a random number between 0 and 1
-function random(){
-    return Math.random();
+//This are test fields, they shouldn't last long
+var names = ["John Smith", "Jane Smith"];
+//var names2 = ["John Jones", "Jane Jones"];
+
+var limit = 0;
+
+//Generates a random number between 0 and the limit
+function random(limit){
+    return Math.round(Math.random()*limit);
 }
 
+//Choose a random name
 function choose(){
-    document.getElementById("name").innerHTML = random();
+    //Get a random name from the list
+    var randomName = names[random(names.length-1)];
+    //Output it
+    document.getElementById("name").innerHTML = randomName;
 }
