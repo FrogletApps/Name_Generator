ebd7c37230819ef3b0afc38f6a72752bf1f75c3a
James Pearson <jap61@kent.ac.uk>
2018-08-22 22:11:42 +0100

    Added titles and randomized forename and surnames

--- NameGenerator.js
+++ NameGenerator.js
@@ -1,6 +1,7 @@
 //This are test fields, they shouldn't last long
-var names = ["John Smith", "Jane Smith"];
-//var names2 = ["John Jones", "Jane Jones"];
+var titles = ["Capt", "Cmdr", "Col", "Cpl", "Dr", "Gen", "Hon", "Lady", "Lord", "Lt", "Lt Col", "Maj", "Master", "Miss", "Mr", "Miss", "Mr", "Mrs", "Ms", "Prof", "Pvt", "Rev", "Rt Hon", "Sgt"]
+var forenames = ["John", "Jane"];
+var surnames = ["Smith", "Jones"];
 
 var limit = 0;
 
@@ -11,8 +12,10 @@ function random(limit){
 
 //Choose a random name
 function choose(){
-    //Get a random name from the list
-    var randomName = names[random(names.length-1)];
+    //Get a random title/name from the list
+    var randomTitle = titles[random(titles.length-1)];
+    var randomForename = forenames[random(forenames.length-1)];
+    var randomSurname = surnames[random(surnames.length-1)];
     //Output it
-    document.getElementById("name").innerHTML = randomName;
+    document.getElementById("name").innerHTML = randomTitle + " " + randomForename + " " + randomSurname;
 }
