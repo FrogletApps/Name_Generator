37e9e76f3303bca609d50eb152ff68b3188f98b3
James Pearson <jap61@kent.ac.uk>
2018-08-22 22:35:45 +0100

    Added test code
    Added jquery and some JSON test code

--- NameGenerator.js
+++ NameGenerator.js
@@ -1,9 +1,16 @@
 //This are test fields, they shouldn't last long
-var titles = ["Capt", "Cmdr", "Col", "Cpl", "Dr", "Gen", "Hon", "Lady", "Lord", "Lt", "Lt Col", "Maj", "Master", "Miss", "Mr", "Miss", "Mr", "Mrs", "Ms", "Prof", "Pvt", "Rev", "Rt Hon", "Sgt"]
+var titles = ["Capt", "Cmdr", "Col", "Cpl", "Dr", "Gen", "Hon", "Lady", "Lord", "Lt", "Lt Col", "Maj", "Master", "Miss", "Mr", "Mrs", "Ms", "Prof", "Pvt", "Rev", "Rt Hon", "Sgt"]
 var forenames = ["John", "Jane"];
 var surnames = ["Smith", "Jones"];
 
 var limit = 0;
+
+//Get titles from JSON - again this is just a test
+$.getJSON("https://spreadsheets.google.com/feeds/list/1SJsdbHnylM69OTft9eiI1AfCJ0PxLkVUNZk-Fq80gT0/od6/public/basic?alt=json", function(result){
+    $.each(result, function(i, field){
+        console.log(field);
+    });
+});
 
 //Generates a random number between 0 and the limit
 function random(limit){
